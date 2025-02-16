1e52217628e50011fa59381522ddfda3e96c526d
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-02-16 08:45:06 +0100

    feat: implement hero section animation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -7,10 +7,72 @@ import Orbit from '@/components/Orbit';
 import Planet from '@/components/Planet';
 import SectionBorder from '@/components/SectionBorder';
 import SectionContent from '@/components/SectionContent';
+import {
+  motion,
+  useMotionValue,
+  useMotionValueEvent,
+  useScroll,
+  useSpring,
+  useTransform,
+} from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
+
+const useMousePosition = () => {
+  const [innerWidth, setInnerWidth] = useState(1);
+  const [innerHeight, setInnerHeight] = useState(1);
+
+  const clientX = useMotionValue(0);
+  const clientY = useMotionValue(0);
+
+  const xProgress = useTransform(clientX, [0, innerWidth], [0, 1]);
+  const yProgress = useTransform(clientY, [0, innerHeight], [0, 1]);
+
+  useEffect(() => {
+    setInnerWidth(window.innerWidth);
+    setInnerHeight(window.innerHeight);
+
+    window.addEventListener('resize', () => {
+      setInnerWidth(window.innerWidth);
+      setInnerHeight(window.innerHeight);
+    });
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', (e) => {
+      clientX.set(e.clientX);
+      clientY.set(e.clientY);
+    });
+  }, []);
+
+  return { xProgress, yProgress };
+};
 
 export const Hero = () => {
+  const { xProgress, yProgress } = useMousePosition();
+
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['end start', 'start end'],
+  });
+
+  const transformedY = useTransform(scrollYProgress, [0, 1], [200, -200]);
+
+  const springX = useSpring(xProgress);
+  const springY = useSpring(yProgress);
+
+  const translateLargeX = useTransform(springX, [0, 1], ['-25%', '25%']);
+  const translateLargeY = useTransform(springY, [0, 1], ['-25%', '25%']);
+
+  const translateMediumX = useTransform(springX, [0, 1], ['-50%', '50%']);
+  const translateMediumY = useTransform(springY, [0, 1], ['-50%', '50%']);
+
+  const translateSmallX = useTransform(springX, [0, 1], ['-200%', '200%']);
+  const translateSmallY = useTransform(springY, [0, 1], ['-200%', '200%']);
+
   return (
-    <section>
+    <section ref={sectionRef}>
       <div className="container">
         <SectionBorder>
           <SectionContent className="relative isolate overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]">
@@ -58,29 +120,60 @@ export const Hero = () => {
             </div>
             <div className="relative isolatem max-w-5xl mx-auto">
               <div className="absolute left-1/2 top-0">
-                <Planet
-                  size="lg"
-                  color="violet"
-                  className="-translate-x-[316px] -translate-y-[76px] rotate-135"
-                />
-                <Planet
-                  size="lg"
-                  color="violet"
-                  className="translate-x-[334px] -translate-y-[188px] -rotate-135"
-                />
-                <Planet
-                  size="sm"
-                  color="fuchsia"
-                  className="-translate-x-[508px] -translate-y-[372px] -rotate-135"
-                />
-                <Planet
-                  size="md"
-                  color="teal"
-                  className="translate-x-[488px] -translate-y-[342px] -rotate-135"
-                />
+                <motion.div
+                  style={{
+                    x: translateLargeX,
+                    y: translateLargeY,
+                  }}
+                >
+                  <Planet
+                    size="lg"
+                    color="violet"
+                    className="-translate-x-[316px] -translate-y-[76px] rotate-135"
+                  />
+                </motion.div>
+                <motion.div
+                  style={{
+                    x: translateLargeX,
+                    y: translateLargeY,
+                  }}
+                >
+                  <Planet
+                    size="lg"
+                    color="violet"
+                    className="translate-x-[334px] -translate-y-[188px] -rotate-135"
+                  />
+                </motion.div>
+                <motion.div
+                  style={{
+                    x: translateSmallX,
+                    y: translateSmallY,
+                  }}
+                >
+                  <Planet
+                    size="sm"
+                    color="fuchsia"
+                    className="-translate-x-[508px] -translate-y-[372px] -rotate-135"
+                  />
+                </motion.div>
+                <motion.div
+                  style={{
+                    x: translateMediumX,
+                    y: translateMediumY,
+                  }}
+                >
+                  <Planet
+                    size="md"
+                    color="teal"
+                    className="translate-x-[488px] -translate-y-[342px] -rotate-135"
+                  />
+                </motion.div>
               </div>
               <div className="absolute top-[30%] left-0 z-10 -translate-x-10 hidden lg:block">
-                <div className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72">
+                <motion.div
+                  className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72"
+                  style={{ y: transformedY }}
+                >
                   <div>
                     Can you generate an incredible frontend application
                     tutorial?
@@ -88,10 +181,13 @@ export const Hero = () => {
                   <div className="text-right text-gray-400 text-sm font-semibold">
                     1m ago
                   </div>
-                </div>
+                </motion.div>
               </div>
               <div className="absolute top-[50%] right-0 z-10 translate-x-10 hidden lg:block">
-                <div className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72">
+                <motion.div
+                  className="bg-gray-800/70 backdrop-blur-md border-gray-700 rounded-xl p-4 w-72"
+                  style={{ y: transformedY }}
+                >
                   <div>
                     <strong>brainwave: </strong> I just created one for you.
                     Check it out!
@@ -99,7 +195,7 @@ export const Hero = () => {
                   <div className="text-right text-gray-400 text-sm font-semibold">
                     Just now
                   </div>
-                </div>
+                </motion.div>
               </div>
               <div className="mt-20 rounded-2xl border-2 overflow-hidden border-gradient relative">
                 <Image src={robotImg} alt="robot image" />
